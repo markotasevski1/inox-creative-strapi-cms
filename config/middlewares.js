196a496020a9e59d365f9e5c140929c888349d24
markotasevski1 <markotasevski123@yahoo.com>
2023-08-08 12:52:09 +0200

    Second Commit

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,13 +6,7 @@ module.exports = [
     config: {
       enabled: true,
       header: "*",
-      origin: [
-        "http://localhost:8080",
-        "http://localhost:3000",
-        "http://localhost:3000/category",
-        "http://127.0.0.1:3000/category",
-        "http://127.0.0.1:3000",
-      ],
+      origin: ["*"],
     },
   },
   "strapi::poweredBy",
